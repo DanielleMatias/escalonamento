64f916949422462232205ffbbd28d22910bda78a
Danielle <danielledanymatias2010@gmail.com>
2017-07-07 15:25:14 -0300

    questao1

---
+++ Questão_01.py
@@ -0,0 +1,15 @@
+#1. Escreva um algoritmo que exiba a seguinte mensagem:
+
+#+----------- +
+
+#+ Be m vi ndo +
+
+#+----------- +
+nome= input('Digite seu nome por favor: ')
+print('+----------- +')
+print('Bem vindo(a) {} +'.format(nome))
+print('+----------- +')
+
+print('Aluna:Danielle dos Santos Matias.')
+input()# caso seja visulaizado pelo comando do idle
+print('Aluna: Danielle dos Santos Matias.')
